d9046fd79a728539edc8fb446e3068e26ba1e09a
s.prem@partner.samsung.com <ecom-s.prem@LT-S-PREM-MAC.local>
2018-06-24 15:24:25 +0530

    signup login api

--- components/signup_login/signup-login-model.js
+++ components/signup_login/signup-login-model.js
@@ -5,7 +5,13 @@
 class SignUpLoginModel {
   constructor (options) {
     this.options = options
-    console.log('signup-login model', options.express)
+    // console.log('signup-login model', options.express)
+  }
+  setHeader (response) {
+    response.setHeader('Access-Control-Allow-Origin', '*')
+    response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE') // If needed
+    response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type') // If needed
+    response.setHeader('Access-Control-Allow-Credentials', true) // If needed
   }
   checkUserExists () {}
   sendOTP () {}
@@ -13,6 +19,11 @@ class SignUpLoginModel {
   updateUserData () {}
   handler (request, response) {
     response.send('hello')
+  }
+
+  validatePhone (request, response) {
+    this.setHeader(response)
+    response.send({sucess: true})
   }
 }
 module.exports = SignUpLoginModel
