528424249aee75e912932abefd8f543dd557d894
s.prem@partner.samsung.com <ecom-s.prem@LT-S-PREM-MAC.local>
2018-08-03 22:30:20 +0530

    Swagger Api Documenation implementation using Express

--- README.md
+++ README.md
@@ -12,3 +12,12 @@ pm2 stop <app_name>
 Node Version 
 ---------------
 Now using node v8.11.3 (npm v5.6.0)
+
+API Documentation [ swagger ]
+------------------
+
+Swagger is used for Api documentation. Please Refer the docs below for detailed instruction
+
+npm i swagger-ui-express -S
+
+https://blog.cloudboost.io/adding-swagger-to-existing-node-js-project-92a6624b855b
---
+++ swagger/exp-swag/index.js
@@ -0,0 +1,23 @@
+var express = require('express')
+var router = express.Router()
+var swaggerUi = require('swagger-ui-express')
+var swaggerDocument = require('./swagger.json')
+
+// // middleware that is specific to this router
+// router.use(function timeLog (req, res, next) {
+//   console.log('Time: ', Date.now())
+//   next()
+// })
+// define the home page route
+router.get('/', function (req, res) {
+  res.send('Birds home page')
+})
+// define the about route
+// router.get('/about', function (req, res) {
+//   res.send('About birds')
+// })
+var app = express()
+// app.use('/', router)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app.use('/api/v1', router)
+app.listen(4000)
